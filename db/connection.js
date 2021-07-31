8bc54442594556d50a0a426522486397aa70d31e
Evan Mora <evanrmora@gmail.com>
2021-07-31 15:47:40 -0600

    final update

--- db/connection.js
+++ db/connection.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql');
 const util = require('util');
+const dotenv = require('dotenv');
 
 const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
-    user: 'root',
-    password: 'Lunath3d0g',
+    user: 'user',
+    password: "public",
     database: 'mancity_DB'
 });
 
